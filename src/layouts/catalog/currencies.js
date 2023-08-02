503059380cec6fe55e407e8abb689fbd670638f3
Stefan <stefan@creative-tim.com>
2023-08-02 15:58:07 +0300

    Create currencies.js

---
+++ src/layouts/catalog/currencies.js
@@ -0,0 +1,86 @@
+/**
+=========================================================
+* Material Dashboard 2 PRO React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-pro-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import { useState } from "react";
+
+// @mui material components
+import Card from "@mui/material/Card";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Divider from "@mui/material/Divider";
+
+// Material Dashboard 2 PRO React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import MDButton from "components/MDButton";
+
+// Material Dashboard 2 PRO React examples
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import DataTable from "examples/Tables/DataTable";
+
+// Data
+import dataTableCurrency from "layouts/catalog/data/dataTableData";
+
+function OrderList() {
+  const [menu, setMenu] = useState(null);
+
+  const openMenu = (event) => setMenu(event.currentTarget);
+  const closeMenu = () => setMenu(null);
+
+  const renderMenu = (
+    <Menu
+      anchorEl={menu}
+      anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+      transformOrigin={{ vertical: "top", horizontal: "left" }}
+      open={Boolean(menu)}
+      onClose={closeMenu}
+      keepMounted
+    >
+      <MenuItem onClick={closeMenu}>Status: Paid</MenuItem>
+      <MenuItem onClick={closeMenu}>Status: Refunded</MenuItem>
+      <MenuItem onClick={closeMenu}>Status: Canceled</MenuItem>
+      <Divider sx={{ margin: "0.5rem 0" }} />
+      <MenuItem onClick={closeMenu}>
+        <MDTypography variant="button" color="error" fontWeight="regular">
+          Remove Filter
+        </MDTypography>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox my={3}>
+        <MDBox display="flex" justifyContent="space-between" alignItems="flex-start" mb={2}>
+          <MDTypography variant="h3" fontWeight="medium">
+            Currencies
+          </MDTypography>
+          <MDButton variant="gradient" color="info">
+            Add Currency
+          </MDButton>
+        </MDBox>
+        <Card>
+          <DataTable table={dataTableCurrency} entriesPerPage={false} canSearch />
+        </Card>
+      </MDBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default OrderList;
