64795a079a3f35f094cd40f94596c4fb0f45701d
Stefan <stefan@creative-tim.com>
2023-08-03 13:51:33 +0300

    Create dataInvoice.js

---
+++ src/layouts/catalog/data/dataInvoice.js
@@ -0,0 +1,38 @@
+/**
+=========================================================
+* Material Dashboard 2 PRO React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-pro-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+/* eslint-disable react/prop-types */
+
+const dataProducts = {
+  columns: [
+    {
+      Header: "Name",
+      accessor: "name",
+    },
+    {
+      Header: "Created",
+      accessor: "created",
+    }
+  ],
+
+  rows: [
+    {
+      name: "Default Template",
+      created: "27 July 2023"
+    },  
+  ],
+};
+
+export default dataProducts;
