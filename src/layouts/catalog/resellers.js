b0c7478a6b04922177bdd6ab53fe3159717e44ad
Stefan <stefan@creative-tim.com>
2023-08-03 13:51:59 +0300

    Create resellers.js

---
+++ src/layouts/catalog/resellers.js
@@ -0,0 +1,62 @@
+/**
+=========================================================
+* Material Dashboard 2 PRO React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-pro-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import { useState } from "react";
+
+// @mui material components
+import Card from "@mui/material/Card";
+
+// Material Dashboard 2 PRO React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import MDButton from "components/MDButton";
+
+// Material Dashboard 2 PRO React examples
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import DataTable from "examples/Tables/DataTable";
+
+// Data
+import dataInvoices from "layouts/catalog/data/dataInvoice";
+
+function OrderList() {
+  const [menu, setMenu] = useState(null);
+
+  const openMenu = (event) => setMenu(event.currentTarget);
+  const closeMenu = () => setMenu(null);
+
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox my={3}>
+        <MDBox display="flex" justifyContent="space-between" alignItems="flex-start" mb={2}>
+          <MDTypography variant="h3" fontWeight="medium">
+            Resellers
+          </MDTypography>
+          <MDButton variant="gradient" color="info">
+            Add Currency
+          </MDButton>
+        </MDBox>
+        <Card>
+          <DataTable table={dataInvoices} entriesPerPage={false} canSearch />
+        </Card>
+      </MDBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default OrderList;
