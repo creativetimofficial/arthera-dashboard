7acdadacf1fe031feffa1c40b7ebb6bc642a30b9
Stefan <stefan@creative-tim.com>
2023-08-03 13:51:36 +0300

    Create dataPlans.js

---
+++ src/layouts/catalog/data/dataPlans.js
@@ -0,0 +1,43 @@
+/**
+=========================================================
+* Material Dashboard 2 PRO React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-pro-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+/* eslint-disable react/prop-types */
+
+const dataProducts = {
+  columns: [
+    {
+      Header: "Name",
+      accessor: "name",
+    },
+    {
+      Header: "Price",
+      accessor: "price",
+    },
+    {
+      Header: "Term",
+      accessor: "term",
+    },
+  ],
+
+  rows: [
+    {
+      name: "Basic",
+      price: "12 $ / month",
+      term: "12 months",
+    },  
+  ],
+};
+
+export default dataProducts;
