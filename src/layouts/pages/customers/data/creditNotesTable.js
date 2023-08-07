23d880f383d2e87f645b62dd4e7057c4764129e8
Stefan <stefan@creative-tim.com>
2023-08-07 15:06:36 +0300

    Create creditNotesTable.js

---
+++ src/layouts/pages/customers/data/creditNotesTable.js
@@ -0,0 +1,50 @@
+/**
+=========================================================
+* Material Dashboard 2 PRO React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-pro-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+/* eslint-disable react/prop-types */
+// ProductsList page components
+
+const dataTableCreditNotes = {
+  columns: [
+    {
+      Header: "ID",
+      accessor: "id",
+    },
+    {
+      Header: "Invoice ID",
+      accessor: "invoiceId",
+    },
+    {
+      Header: "Created On",
+      accessor: "createdOn",
+    },
+    {
+      Header: "Amount",
+      accessor: "amount",
+    },
+  ],
+
+  rows: [
+    {
+      id: "d981abbb-3622-487-9564-d594c9d04f83",
+      invoiceId: "g981obbb-3699.4870-9564-d594c9d04f83",
+      createdOn: "27 Jul 2023 10:30",
+      amount: "100 AA",
+    },
+    
+  ],
+};
+
+export default dataTableCreditNotes;
