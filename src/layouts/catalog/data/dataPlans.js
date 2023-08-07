0b929a6eb7c46601aed7d07012cb81f3d2d03abb
Stefan <stefan@creative-tim.com>
2023-08-07 15:06:17 +0300

    Update dataPlans.js

--- src/layouts/catalog/data/dataPlans.js
+++ src/layouts/catalog/data/dataPlans.js
@@ -14,6 +14,11 @@ Coded by www.creative-tim.com
 */
 
 /* eslint-disable react/prop-types */
+import Link from "@mui/material/Link";
+
+import MDBox from "components/MDBox";
+
+import MDButton from "components/MDButton";
 
 const dataProducts = {
   columns: [
@@ -29,6 +34,10 @@ const dataProducts = {
       Header: "Term",
       accessor: "term",
     },
+    {
+      Header: "",
+      accessor: "details",
+    },
   ],
 
   rows: [
@@ -36,6 +45,8 @@ const dataProducts = {
       name: "Basic",
       price: "12 $ / month",
       term: "12 months",
+      details: <MDBox display="flex"><Link href="/catalog/plan"><MDButton variant="text" color="primary">View</MDButton></Link><Link href="/catalog/plan"><MDButton variant="text" color="primary">Delete</MDButton></Link></MDBox>
+
     },  
   ],
 };
