e9a6a61a89cd9b141ade8c11c1fedfe73471229f
Stefan <stefan@creative-tim.com>
2023-08-07 17:27:32 +0300

    Create dataProduct.js

---
+++ src/layouts/catalog/data/dataProduct.js
@@ -0,0 +1,28 @@
+/**
+=========================================================
+* Material Dashboard 2 PRO React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-pro-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+const selectData = {
+  category: ["BASE", "ADDON", "STANDALONE"],
+  product: [
+    "Product 1",
+    "Product 2",
+  ],
+  billing: ["IN_ADVANCE"],
+  phase: ["TRIAL", "DISCOUNT", "FIXEDTERM", "EVERGREEN"],
+  duration: ["DAYS", "MONTHS", "YEARS"],
+
+};
+
+export default selectData;
