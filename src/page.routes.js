3c6808a880e9910f1d89e9114becd02d877e6b74
Stefan <stefan@creative-tim.com>
2023-08-08 16:44:30 +0300

    Update page.routes.js

--- src/page.routes.js
+++ src/page.routes.js
@@ -40,24 +40,14 @@ const pageRoutes = [
     rowsPerColumn: 2,
     collapse: [
       {
-        name: "dashboards",
+        name: "dashboard",
         icon: <Icon>dashboard</Icon>,
         collapse: [
           {
-            name: "analytics",
+            name: "home",
             route: "/home",
           },
       
-        ],
-      },
-      {
-        name: "users",
-        icon: <Icon>people</Icon>,
-        collapse: [
-          {
-            name: "reports",
-            route: "/pages/users/reports",
-          },
         ],
       },
       {
@@ -65,15 +55,15 @@ const pageRoutes = [
         icon: <Icon>queue_play_next</Icon>,
         collapse: [
           {
-            name: "pricing page",
-            route: "/pages/pricing-page",
+            name: "customers",
+            route: "/customers",
           },
-          { name: "RTL", route: "/pages/rtl" },
-          { name: "widgets", route: "/pages/widgets" },
-          { name: "charts", route: "/pages/charts" },
+          { name: "checkout", route: "/catalog/checkout" },
+          { name: "currencies", route: "/catalog/currencies" },
+          { name: "invoices", route: "/catalog/invoices" },
           {
-            name: "notfications",
-            route: "/pages/notifications",
+            name: "plans",
+            route: "/catalog/plans",
           },
         ],
       },
@@ -82,8 +72,8 @@ const pageRoutes = [
         icon: <Icon>precision_manufacturing</Icon>,
         collapse: [
           {
-            name: "timeline",
-            route: "/pages/projects/timeline",
+            name: "products",
+            route: "/catalog/products",
           },
         ],
       },
@@ -92,33 +82,11 @@ const pageRoutes = [
         icon: <Icon>account_balance</Icon>,
         collapse: [
           {
-            name: "settings",
-            route: "/pages/account/setting",
-          },
-          {
-            name: "billing",
-            route: "/pages/account/billing",
-          },
-          {
-            name: "invoice",
-            route: "/pages/account/invoice",
+            name: "Setup Guide",
+            route: "/guide",
           },
         ],
-      },
-      {
-        name: "profile",
-        icon: <Icon>badge</Icon>,
-        collapse: [
-          {
-            name: "profile overview",
-            route: "/pages/profile/profile-overview",
-          },
-          {
-            name: "all projects",
-            route: "/pages/profile/all-projects",
-          },
-        ],
-      },
+      }
     ],
   },
   {
@@ -137,10 +105,6 @@ const pageRoutes = [
             name: "cover",
             route: "/authentication/sign-in/cover",
           },
-          {
-            name: "illustration",
-            route: "/authentication/sign-in/illustration",
-          },
         ],
       },
       {
@@ -154,112 +118,9 @@ const pageRoutes = [
           },
         ],
       },
-      {
-        name: "reset password",
-        dropdown: true,
-        icon: <Icon>restart_alt</Icon>,
-        collapse: [
-          {
-            name: "cover",
-            route: "/authentication/reset-password/cover",
-          },
-        ],
-      },
     ],
   },
-  {
-    name: "application",
-    collapse: [
-      {
-        name: "kanban",
-        route: "/applications/kanban",
-        icon: "widgets",
-      },
-      {
-        name: "wizard",
-        route: "/applications/wizard",
-        icon: "import_contacts",
-      },
-      {
-        name: "data tables",
-        route: "/applications/data-tables",
-        icon: "backup_table",
-      },
-      {
-        name: "calendar",
-        route: "/applications/calendar",
-        icon: "event",
-      },
-    ],
-  },
-  {
-    name: "ecommerce",
-    columns: 2,
-    rowsPerColumn: 1,
-    collapse: [
-      {
-        name: "orders",
-        icon: <Icon>shopping_cart</Icon>,
-        collapse: [
-          {
-            name: "order list",
-            route: "/ecommerce/orders/order-list",
-          },
-          {
-            name: "order details",
-            route: "/ecommerce/orders/order-details",
-          },
-        ],
-      },
-      {
-        name: "products",
-        icon: <Icon>memory</Icon>,
-        collapse: [
-          {
-            name: "new product",
-            route: "/ecommerce/products/new-product",
-          },
-          {
-            name: "edit product",
-            route: "/ecommerce/products/edit-product",
-          },
-          {
-            name: "product page",
-            route: "/ecommerce/products/product-page",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: "docs",
-    collapse: [
-      {
-        name: "getting started",
-        href: "https://www.creative-tim.com/learning-lab/react/quick-start/material-dashboard/",
-        description: "All about overview, quick start, license and contents",
-        icon: <Icon>article</Icon>,
-      },
-      {
-        name: "foundation",
-        href: "https://www.creative-tim.com/learning-lab/react/colors/material-dashboard/",
-        description: "See our colors, icons and typography",
-        icon: <Icon>grading</Icon>,
-      },
-      {
-        name: "components",
-        href: "https://www.creative-tim.com/learning-lab/react/alerts/material-dashboard/",
-        description: "Explore our collection of fully designed components",
-        icon: <Icon>apps</Icon>,
-      },
-      {
-        name: "plugins",
-        href: "https://www.creative-tim.com/learning-lab/react/datepicker/material-dashboard/",
-        description: "Check how you can integrate our plugins",
-        icon: <Icon>extension</Icon>,
-      },
-    ],
-  },
+ 
 ];
 
 export default pageRoutes;
